a1d23213c1010b5fa3341c4b11a349936cf44b99
Rashmi Yadav <rashmi.yadav@mynewsdesk.com>
2015-02-16 16:57:55 +0100

    Update to create shortcut of path

--- zsh_profile.d/path_shortcut.zsh
+++ zsh_profile.d/path_shortcut.zsh
@@ -1,26 +1,13 @@
-export CURRENT_PROJECT_PATH=$HOME/.current-project
 export ALIASFILE=$HOME/checkouts/dotfiles/aliasrc
 
 function short_path {
   if  [ $2 ]
   then
-    echo "path is $2"
-    echo "$ALIASFILE"
+    echo "with argumentsssssssssssssss"
     echo "alias $1='cd $2'" >> "$ALIASFILE"
-
-  elif [[ -f $CURRENT_PROJECT_PATH ]]
-  then
-    echo "curretn path"
-    alias $1="cd $CURRENT_PROJECT_PATH"
   else
-    echo "You are not allowed to create this"
+    echo "alias $1='cd `pwd`'" >> "$ALIASFILE"
   fi
 
   echo "created shortcut for $1"
-}
-
-current() {
-  if [[ -f $CURRENT_PROJECT_PATH ]]; then
-    cd "$(cat $CURRENT_PROJECT_PATH)"
-  fi
 }
